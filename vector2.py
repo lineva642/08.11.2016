797ebced4435afe63eaa711fa1b09b3f90f2f0b5
Lineva <96028@mail.ru>
2016-11-08 19:59:51 +0300

    Create vector2.py

---
+++ vector2.py
@@ -0,0 +1,22 @@
+class Vector:
+    def __init__(self, x, y):
+            self.x=x
+            self.y=y
+    def __str__(self):
+        return str(self.x)+', ' + str(self.y)
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y + other.y)
+
+n=int(input())
+a=[]
+m=0
+im=0
+for i in range(n):
+    a.append(Vector(input()))
+    if float(abs(a[i])) > m:
+        m=abs(a[i])
+        im=i
+print('Наибольшее расстояние:',a[im])
